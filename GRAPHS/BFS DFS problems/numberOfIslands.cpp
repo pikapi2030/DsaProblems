6937d0089cccadc8ce89e20995451318cb9e8e4a
paras <pikapi2030@gmail.com>
2025-03-10 11:10:13 +0530

    snake and ladder

--- GRAPHS/BFS DFS problems/numberOfIslands.cpp
+++ GRAPHS/BFS DFS problems/numberOfIslands.cpp
@@ -1,12 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 /*
-Given a grid of size n*m (n is the number of rows and m is the number of columns in the grid)
-consisting of '0's (Water) and '1's(Land). Find the number of islands.
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), 
+return the number of islands.
 
-Note: An island is either surrounded by water or the boundary of a grid and is formed
-by connecting adjacent lands horizontally or vertically or diagonally i.e.,
-!in all 8 directions.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or
+vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ 
+
+Example 1:
+
+Input: grid = [
+["1","1","1","1","0"],
+["1","1","0","1","0"],
+["1","1","0","0","0"],
+["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+
+Input: grid = [
+["1","1","0","0","0"],
+["1","1","0","0","0"],
+["0","0","1","0","0"],
+["0","0","0","1","1"]
+]
+Output: 3
+
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+
 */
 
 void DFSER(vector<vector<char>> &grid, vector<vector<int>> &vis, const int drow[], const int dcol[], int row, int col)
@@ -14,11 +45,11 @@ void DFSER(vector<vector<char>> &grid, vector<vector<int>> &vis, const int drow[
     vis[row][col] = 1;
     int m = grid.size();
     int n = grid[0].size();
-    for (int i = 0; i < 8; i++) // 8 possible directions
+    for (int i = 0; i < 4; i++) // 4 possible directions
     {
         int nbrow = row + drow[i];
         int nbcol = col + dcol[i];
-        if (grid[nbrow][nbcol] == 1 && vis[nbrow][nbcol] == 0 && nbrow > 0 && nbcol > 0 && nbrow < m && nbcol < n)
+        if ( nbrow >= 0 && nbcol >= 0 && nbrow < m && nbcol < n&& grid[nbrow][nbcol] == '1' && vis[nbrow][nbcol] == 0 )
         {
             DFSER(grid, vis, drow, dcol, nbrow, nbcol);
         }
@@ -29,18 +60,20 @@ int numIslands(vector<vector<char>> &grid)
 {
     int m = grid.size();
     int n = grid[0].size();
-    int drow[] = {-1,1,0,0};
-    int dcol[] = {};
+    int drow[] = {1,0,-1,0};
+    int dcol[] = {0,1,0,-1};
     vector<vector<int>> vis(m, vector<int>(n, 0));
     int islandcount = 0; // ans
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            if (grid[i][j] == 1 && vis[i][j] == 0)
+            if (grid[i][j] == '1' && vis[i][j] == 0)
             {
+                islandcount++;
                 DFSER(grid, vis, drow, dcol, i, j);
             }
         }
     }
+    return islandcount;
 }
--- GRAPHS/Weighted Graphs/FordFulkerson/DFS.cpp
+++ GRAPHS/Weighted Graphs/FordFulkerson/DFS.cpp
@@ -7,14 +7,14 @@ int dfs(int start, int sink, vector<vector<int>> &residual, vector<int> &vis, in
 	{
 		return flow;
 	}
-	vis[start] = 1;
+	vis[start]=1;
 	for (int i = 0; i < residual.size(); i++)
 	{
 		if (residual[start][i] > 0 && !vis[i])
 		{
 			int newflow = min(flow, residual[start][i]);
-
-			int bottleNeck = dfs(i, sink, residual, vis, newflow);
+			vis[i] = 1;
+			int bottleNeck =  dfs(i, sink, residual, vis, newflow);
 			if (bottleNeck > 0)
 			{
 				residual[start][i] -= bottleNeck;
@@ -31,16 +31,16 @@ int fordFulkerson(vector<vector<int>> &graph, int sink, int source)
 	int n = graph.size();
 	vector<vector<int>> residualGraph = graph;
 	int maxFlow = 0;
-
-	while (true)
+	
+	while(true)
 	{
-		vector<int> vis(n, 0);
-		int flow = dfs(source, sink, residualGraph, vis, INT_MAX);
-		if (flow == 0)
+		vector<int>vis(n,0);
+		int flow=dfs(source,sink,residualGraph,vis,INT_MAX);
+		if(flow==0)
 		{
 			break;
 		}
-		maxFlow += flow;
+		maxFlow+=flow;
 	}
 	return maxFlow;
 }
@@ -48,16 +48,17 @@ int fordFulkerson(vector<vector<int>> &graph, int sink, int source)
 int main()
 {
 	int n;
-	cout << "Enter size:-";
-	cin >> n;
-	vector<vector<int>> v(n, vector<int>(n));
-	for (int i = 0; i < n; i++)
+	cout<<"Enter size:-";
+	cin>>n;
+	vector<vector<int>>v(n,vector<int>(n));
+	for(int i=0;i<n;i++)
 	{
-		cout << "ENTER ROW:" << endl;
-		for (int j = 0; j < n; j++)
+		cout<<"ENTER ROW:"<<endl;
+		for(int j=0;j<n;j++)
 		{
-			cin >> v[i][j];
+         cin>>v[i][j]; 
+		 
 		}
 	}
-	cout << fordFulkerson(v, 2, 0);
+	cout<<fordFulkerson(v,2,0);
 }
