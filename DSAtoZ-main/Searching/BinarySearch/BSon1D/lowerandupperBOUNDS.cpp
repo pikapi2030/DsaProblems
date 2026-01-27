24eeebd06f0c4e9705ef09fbbc367873fe494892
paras <pikapi2030@gmail.com>
2026-01-27 13:28:04 +0530

    formating changes

--- DSAtoZ-main/Searching/BinarySearch/BSon1D/lowerandupperBOUNDS.cpp
+++ DSAtoZ-main/Searching/BinarySearch/BSon1D/lowerandupperBOUNDS.cpp
@@ -1,40 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 /*lower bound algo
 
-std::lower_bound is a Standard Template Library (STL) algorithm used to find the 
-first position where a given value can be inserted in a sorted range without 
-violating the order. In simple terms, it returns an iterator pointing to the first 
+std::lower_bound is a Standard Template Library (STL) algorithm used to find the
+first position where a given value can be inserted in a sorted range without
+violating the order. In simple terms, it returns an iterator pointing to the first
 element that is greater than or equal to (>=) the given value.
 */
 
-
-int LB(vector<int>v,int target)
+int LB(vector<int> v, int target)
 {
-    int n=v.size();
-    int low=0;
-    int high=n-1;
-    int ans=n;
-    while(high>=low)
+    int n = v.size();
+    int low = 0;
+    int high = n - 1;
+    int ans = n;
+    while (high >= low)
     {
-            int mid=(low+high)/2;
+        int mid = (low + high) / 2;
 
-        if(v[mid]>=target)
+        if (v[mid] >= target)
         {
-            ans=mid;
-            high=mid-1;
+            ans = mid;
+            high = mid - 1;
         }
-        else 
+        else
         {
-            low=mid+1;
+            low = mid + 1;
         }
     }
     return ans;
 }
 
 /*LOWER bound function in stl
-lower_bound(v.begin(),v.end(),target) return and iterator pointing 
+lower_bound(v.begin(),v.end(),target) return and iterator pointing
 towards lower bound of the target
 
 to get index from the function we can simply subtract v.begin() from the iterator
@@ -43,30 +42,30 @@ int lb_index=lower_bound(v.begin(),v.end())-v.begin();
 
 */
 
-
 /*upper bound
-upper_bound is a Standard Template Library (STL) function used to find the first element 
-that is strictly greater than a given value in a sorted range. It is commonly used for 
+upper_bound is a Standard Template Library (STL) function used to find the first element
+that is strictly greater than a given value in a sorted range. It is commonly used for
 efficient searching and range queries in sorted containers.*/
 
-int UB(vector<int>v,int target)
+int UB(vector<int> v, int target)
 {
-    int n=v.size();
-    int low=0;
-    int high=n-1;
-    int ans=n;
-    while(high>=low)
-    {
-            int mid=(low+high)/2;
 
-        if(v[mid]>target)//here is the difference
+    int n = v.size();
+    int low = 0;
+    int high = n - 1;
+    int ans = n;
+    while (high >= low)
+    {
+        int mid = (low + high) / 2;
+
+        if (v[mid] > target) // here is the difference
         {
-            ans=mid;
-            high=mid-1;
+            ans = mid;
+            high = mid - 1;
         }
-        else//v[mid]<=target 
+        else // v[mid]<=target
         {
-            low=mid+1;
+            low = mid + 1;
         }
     }
     return ans;
