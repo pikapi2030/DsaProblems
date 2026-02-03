2a51bdde716086b8aea54b2efaf4d59d241987c0
paras <pikapi2030@gmail.com>
2026-02-03 18:24:59 +0530

    corrected code

--- DP/Subsequences/Problems/PartionArrayIntoSubset.cpp
+++ DP/Subsequences/Problems/PartionArrayIntoSubset.cpp
@@ -1,86 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
-/*
-You are given an array 'arr' containing 'n' non-negative integers.
 
+class Solution {
+public:
+    // Function to find the minimum absolute difference between two subset sums
+    int minSubsetSumDifference(vector<int>& arr, int n) {
+        int totSum = 0;
 
-Your task is to partition this array into two subsets such that the absolute difference between subset sums is minimum.
+        // Calculate the total sum of the array
+        for (int i = 0; i < n; i++) {
+            totSum += arr[i];
+        }
 
+        // Initialize a DP table to store the results of the subset sum problem
+        vector<vector<bool>> dp(n, vector<bool>(totSum + 1, false));
 
-You just need to find the minimum absolute difference considering any valid division of the array elements.
+        // Base case: If no elements are selected (sum is 0), it's a valid subset
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
 
+        // Initialize the first row based on the first element of the array
+        if (arr[0] <= totSum)
+            dp[0][arr[0]] = true;
 
-Note:
+        // Fill in the DP table using a bottom-up approach
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= totSum; target++) {
+                // Exclude the current element
+                bool notTaken = dp[ind - 1][target];
 
-1. Each array element should belong to exactly one of the subsets.
+                // Include the current element if it doesn't exceed the target
+                bool taken = false;
+                if (arr[ind] <= target)
+                    taken = dp[ind - 1][target - arr[ind]];
 
-2. Subsets need not always be contiguous.
-For example, for the array : [1, 2, 3], some of the possible divisions are
-   a) {1,2} and {3}
-   b) {1,3} and {2}.
-
-3. Subset-sum is the sum of all the elements in that subset.
-
-Example:
-
-Input: 'n' = 5, 'arr' = [3, 1, 5, 2, 8].
-
-Ouput: 1
-
-Explanation: We can partition the given array into {3, 1, 5} and {2, 8}.
-This will give us the minimum possible absolute difference i.e. (10 - 9 = 1).
-
-
-*/
-
-/*aproach
-pre-requisite:- subSetSumK by tabulation
-if we notice what does our tabulation table in subSetSumK tells,
-DP[i][j] means if  sum J is possible till ith index
-now if we want to minimise the absolute difference of partition, we will try to part it in totalsum/2
-subsets, and check for the closest to J halfSum  till (n-1)th index which is possible in our DP matrix
-*/
-int minimumDifference(vector<int> &nums)
-{
-    int n = nums.size();
-    int totalSum = 0;
-    for (auto it : nums)
-    {
-        totalSum += it;
-    }
-    int halfSum = totalSum / 2;
-
-    vector<vector<bool>> dp(n, vector<bool>(halfSum + 1, false));
-    for (int i = 0; i < n; i++)
-    {
-        dp[i][0] = true;
-    }
-    if (halfSum >= nums[0])
-    {
-        dp[0][nums[0]] = true;
-    }
-
-    for (int index = 1; index < n; index++)
-    {
-        for (int target = 1; target <= halfSum; target++)
-        {
-            bool pick = false;
-            if (target >= nums[index])
-            {
-                pick = dp[index - 1][target - nums[index]];
+                dp[ind][target] = notTaken || taken;
             }
-            bool notPick = dp[index - 1][target];
-            dp[index][target] = pick || notPick;
         }
-    }
-    int SumClosestToHalfSum = 0;
-    for (int target = halfSum; target >= 0; target--) // travesing last row of dp array in reverse
-    {
-        if (dp[n - 1][target] == true) // first TRUE from reverse(larget possible sum till (n-1)th index)
-        {
-            SumClosestToHalfSum = target;
-            break;
+
+        int mini = 1e9;
+        for (int i = 0; i <= totSum; i++) {
+            if (dp[n - 1][i] == true) {
+                // Calculate the absolute difference between two subset sums
+                int diff = abs(i - (totSum - i));
+                mini = min(mini, diff);
+            }
         }
+        return mini;
     }
-    return totalSum - (2 * SumClosestToHalfSum);
+};
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4};
+    int n = arr.size();
+
+    Solution sol;
+    cout << "The minimum absolute difference is: " << sol.minSubsetSumDifference(arr, n);
+
+    return 0;
 }
